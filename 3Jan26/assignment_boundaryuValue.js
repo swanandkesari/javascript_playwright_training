cde41a4fd8108e412cdad8ac6f0a01c762f591c7
Swanand Kesari <swanandkesari@gmail.com>
2026-01-04 17:37:02 +0530

    imporved answer about assignment about operators

--- 3Jan26/assignment_boundaryuValue.js
+++ 3Jan26/assignment_boundaryuValue.js
@@ -1,12 +1,18 @@
-/* THere is an application which accepts age as one inpit from user
+/* There is an application which accepts age as one input from user
 Application should accept age between 18 to 60 both inclusive
 then log "Valid age range" else log "age must be between 18 to 60"
 */
 let age = 12 // 18;
-
+console.log("Age entered: " + age);
 if (age >= 18 && age <= 60) {
     console.log("Valid age range");
 }
+else if (age > 60) {
+    console.log("age is above 60. The individual is senior citizen")
+}
+else if (age < 18 && age > 0) {
+    console.log("age is below 18. Induvidual is minor")
+}
 else {
-    console.log("age must be between 18 to 60");
+    console.log("age must be between 18 to 60. Please enter valid age");
 }
